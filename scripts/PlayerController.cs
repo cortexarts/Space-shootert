c6dfa51adcf3d906568ec29683034b55a9d9ef62
Danoontjen <daan.jansen-@hotmail.com>
2015-12-06 13:13:53 +0100

    updates blender
    bar enzo

--- scripts/PlayerController.cs
+++ scripts/PlayerController.cs
@@ -33,7 +33,7 @@ public class PlayerController : MonoBehaviour
 	{
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
-		Debug.Log ("Horizontal: " + moveHorizontal, gameObject);
+		//Debug.Log ("Horizontal: " + moveHorizontal, gameObject);
 
 		
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
