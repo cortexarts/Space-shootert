58d49ea1ddb632b467943dccdeca6bb433fe8eb8
Danoontjen <daan.jansen-@hotmail.com>
2016-01-03 13:18:43 +0100

    Alpha space shooter

---
+++ Assets/DestroybyContact.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroybyContact : MonoBehaviour {
+
+	public GameObject explosion;
+	public GameObject playerExplosion;
+
+
+	void OnTriggerEnter(Collider other) 
+	{
+		Debug.Log ("OnTriggerEnterDestroybyContact");
+		if (other.tag == "Boundary")
+		{
+			Debug.Log ("OnTriggerEnterBoundary");
+		}
+
+		Debug.Log (other.tag);
+		if (other.tag == "Player")
+		{
+			Debug.Log ("OnTriggerEnterplayer");
+			Instantiate(explosion, transform.position, transform.rotation);
+			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+			Destroy(other.gameObject);
+			Destroy(gameObject);
+		}
+	}
+}
--- Assets/Done/Done_Scripts/Done_Mover.cs
+++ Assets/Done/Done_Scripts/Done_Mover.cs
@@ -4,9 +4,14 @@ using System.Collections;
 public class Done_Mover : MonoBehaviour
 {
 	public float speed;
+	private float startY;
 
 	void Start ()
 	{
+		startY = rigidbody.position.y;
 		rigidbody.velocity = transform.forward * speed;
+	}
+	void Update() {
+		rigidbody.position = new Vector3 (rigidbody.position.x,startY, rigidbody.position.z);
 	}
 }
