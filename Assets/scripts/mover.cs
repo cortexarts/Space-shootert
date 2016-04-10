7210d46e2d137f125f59d6344f7d445cbbb2d7b6
Danoontjen <daan.jansen-@hotmail.com>
2016-04-10 14:01:54 +0200

    recovered spawning tapir
    20160410

--- Assets/scripts/mover.cs
+++ Assets/scripts/mover.cs
@@ -4,9 +4,14 @@ using System.Collections;
 public class mover : MonoBehaviour 
 {
 	public float speed; 
-	void start ()
-	{
-				rigidbody.velocity = transform.forward * speed;
+	void FixedUpdate  () {
+				//rigidbody.velocity = transform.forward * speed;
+		rigidbody.position = new Vector3 
+			(
+				rigidbody.position.x, 
+				0.0f, 
+				rigidbody.position.z - (speed / 100.0f)
+			);
 
-		}
+	}
 }
